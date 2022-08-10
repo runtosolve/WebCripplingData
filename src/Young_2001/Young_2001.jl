85d515e38ff41dcce7390c73268f620b2df3998c
Shangqun Zhou <shangqun.zhou@runtosolve.com>
2022-08-10 10:34:04 -0400

    young update

---
+++ src/Young_2001/Young_2001.jl
@@ -0,0 +1,16 @@
+using CSV
+using DataFrames
+using JSON
+using Base64
+using StringEncodings
+using DataStructures
+using JSONTables
+
+# Data = CSV.read(pkgdir(WebCripplingData, "src", "Young_2001.csv"), DataFrame)
+Data = CSV.File(open(read, "src/Young 2001.csv")) |> DataFrame
+# Data = CSV.File(open(read, "./Data.csv", enc"GB18030")) |> DataFrame
+all_data_json = [OrderedDict(d[1] => d[2] for d in zip(names(Data), Data[item, :]))  for item in 1:size(Data)[1]]
+stringdata = JSON.json(all_data_json, 4)
+open("write_read.json", "w") do f
+    write(f, stringdata)
+ end
